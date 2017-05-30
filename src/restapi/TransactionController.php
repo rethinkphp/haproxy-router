e0317044e68cd2946e702c8c0f8a08ecc8c7cd37
Jin Hu <bixuehujin@gmail.com>
2017-05-31 01:00:33 +0800

    Define routes

---
+++ src/restapi/TransactionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace rethink\hrouter\restapi;
+
+use blink\core\Object;
+
+/**
+ * TransactionController class
+ */
+class TransactionController extends Object
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function view()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+}
--- src/restapi/routes.php
+++ src/restapi/routes.php
@@ -1,4 +1,19 @@
 <?php
 return [
-    ['GET', '/', 'IndexController@sayHello']
+    ['GET', '/', 'IndexController@sayHello'],
+
+    ['GET', '/services', 'ServiceController@index'],
+    ['POST', '/services', 'ServiceController@create'],
+    ['GET', '/services/:id', 'ServiceController@view'],
+    ['PUT', '/services/:id', 'ServiceController@update'],
+    ['DELETE', '/services/:id', 'ServiceController@delete'],
+
+    ['GET', '/services/:id/servers', 'ServerController@index'],
+    ['POST', '/services/:id/servers', 'ServerController@create'],
+    ['GET', '/services/:id/servers/:serverId', 'ServerController@view'],
+    ['PUT', '/services/:id/servers/:serverId', 'ServerController@update'],
+    ['DELETE', '/services/:id/servers/:serverId', 'ServerController@delete'],
+
+    ['POST', '/transactions', 'TransactionController@create'],
+    ['PUT', '/transactions', 'TransactionController@update'],
 ];
