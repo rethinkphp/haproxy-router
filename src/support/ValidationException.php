c64c64b91af8c6317d337ebd53ec5736f64d6cdc
Jin Hu <bixuehujin@gmail.com>
2017-06-07 23:28:34 +0800

    Added ValidationException class

---
+++ src/support/ValidationException.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace rethink\hrouter\support;
+
+use blink\core\Exception;
+use blink\support\Json;
+use blink\core\InvalidParamException;
+
+/**
+ * Class ValidationException
+ *
+ * @package chalk\base
+ */
+class ValidationException extends Exception
+{
+    public $errors;
+
+    public function __construct($errors, $code = 0, $previous = null)
+    {
+        $this->errors = $errors;
+
+        parent::__construct('Validation Error: ' . Json::encode($errors, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), $code, $previous);
+    }
+
+    /**
+     * Create a ValidationException from the arguments list.
+     *
+     * @param $field
+     * @param $error
+     * @return static
+     */
+    public static function fromArgs($field, $error)
+    {
+        $args = func_get_args();
+
+        if (count($args) % 2 !== 0) {
+            throw new InvalidParamException('The total arguments must be multiples of 2');
+        }
+
+        $errors = [];
+
+        foreach (array_chunk($args, 2) as list($key, $error)) {
+            $error = static::normalizeError($error);
+            $error['field'] = $key;
+
+            $errors[] = $error;
+        }
+
+        return new static($errors);
+    }
+
+    protected static function normalizeError($error)
+    {
+        if (!is_array($error)) {
+            $error = ['message' => (string)$error];
+        }
+
+        return $error;
+    }
+
+    /**
+     * Create a ValidationException from raw array.
+     *
+     * @param array $errors
+     * @return static
+     */
+    public static function fromArray(array $errors)
+    {
+        return new static($errors);
+    }
+}
